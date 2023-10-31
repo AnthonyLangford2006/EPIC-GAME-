1055a9bf0c71cde03ea348015f4168ede365e886
AnthonyLangford2006 <146819132+AnthonyLangford2006@users.noreply.github.com>
2023-10-31 13:12:56 +0000

    Added animation to player

---
+++ Assets/Script/Helper.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    LayerMask groundLayerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundLayerMask = LayerMask.GetMask("Ground");
+
+
+
+    }
+
+    // Update is called once per frame
+    public void FlipObject(bool flip)
+    {
+
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+
+        if (flip == true)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+
+    public bool ExtendedRayCollisionCheck(float xoffs, float yoffs)
+    {
+        float rayLength = 0.5f;
+        bool hitSomething = false;
+
+
+        Vector3 offset = new Vector3(xoffs, yoffs, 0);
+
+
+        RaycastHit2D hit;
+
+
+        hit = Physics2D.Raycast(transform.position + offset, -Vector2.up, rayLength, groundLayerMask);
+
+        Color hitColor = Color.white;
+
+
+        if (hit.collider != null)
+        {
+            print("Player has collided with Ground layer");
+            hitColor = Color.green;
+            hitSomething = true;
+        }
+
+        Debug.DrawRay(transform.position + offset, -Vector2.up * rayLength, hitColor);
+
+        return hitSomething;
+
+    }
+
+}
---
+++ Assets/Script/gameCode.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class THEEPICGAMECODE : MonoBehaviour
+{
+    Animator anim;
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+    Helper helper;
+    public GameObject weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        helper = gameObject.AddComponent<Helper>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Jump", false);
+        anim.SetBool("Run", false);
+        int speed = 1;
+
+        DoRun();
+
+        void DoRun()
+        {
+            if (Input.GetKey("a") == true)
+            {
+               
+                transform.position = new Vector2(transform.position.x - (speed * Time.deltaTime), transform.position.y);
+                anim.SetBool("Run", true);
+                sr.flipX = true;
+               
+            }
+
+
+            if (Input.GetKey("d") == true)
+            {
+               
+                transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
+                anim.SetBool("Run", true);
+                sr.flipX = false;
+            }
+        }
+        int jumpAmount = 2;
+        
+        bool hit;
+        if (Input.GetKeyDown("w"))
+        {
+            hit = helper.ExtendedRayCollisionCheck(0, 0.1f);
+            if (hit == true)
+            {
+                rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+                anim.SetBool("Jump", true);
+                
+                
+
+            }
+        }
+
+
+        if (Input.GetKeyDown("q"))
+        {
+            print("player pressed attack");
+           
+        }
+        int moveDirection = 1;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the bullet at the position and rotation of the player
+            GameObject clone;
+            clone = Instantiate(weapon, transform.position, transform.rotation);
+
+
+            // get the rigidbody component
+            Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
+
+
+            // set the velocity
+            rb.velocity = new Vector3(15 * moveDirection, 0, 0);
+
+
+            // set the position close to the player
+            rb.transform.position = new Vector3(transform.position.x, transform.position.y +
+            2, transform.position.z + 1);
+        }
+
+
+
+    }
+}
